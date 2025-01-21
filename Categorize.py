22fa3d1493b2715f104a4ba18af8682ac7da14c9
erfan ramezani <erfan.ramezany22@gmail.com>
2025-01-21 10:37:49 +0330

    commited

---
+++ Categorize.py
@@ -0,0 +1,47 @@
+import ollama
+import os
+
+
+model = "llama3.2:1b"
+
+input_file = "C:/Users/ASUS-X515EP/Desktop/ollama/list-of-item.txt"
+output_file = "C:/Users/ASUS-X515EP/Desktop/ollama/categorized-list.txt"
+
+
+# Check if input file exists
+if not os.path.exists(input_file):
+    print(f"Input file '{input_file}' does not exist.")
+    exit(1)
+   
+
+
+with open(input_file, 'r') as f:
+    items = f.read().strip()
+   
+   
+
+prompt = f"""
+
+
+Here is your list:
+
+{items}
+
+Please:
+
+1.categorize them into dairy and vegtables and foods
+2.write down benefits of each one of them 
+
+"""
+try:
+    response = ollama.generate(model=model, prompt=prompt)
+    generated_text = response.get("response", " ")  
+
+    
+    with open(output_file, 'w') as f:
+        f.write(generated_text.strip())
+
+    print(f"Categorized list has been saved to '{output_file}'.")
+    #print (f" this is your list : {generated_text}")
+except Exception as e:
+    print("An error happens:", str(e))
